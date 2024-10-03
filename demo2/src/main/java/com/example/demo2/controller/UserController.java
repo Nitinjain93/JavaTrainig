1a84be6c81e45f1dd96f846f6baac22c51364379
Nitinjain93 <nitinjain9319@gmail.com>
2024-10-03 09:16:33 +0530

    initial commit

--- demo2/src/main/java/com/example/demo2/controller/UserController.java
+++ demo2/src/main/java/com/example/demo2/controller/UserController.java
@@ -7,8 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @RestController
 @RequestMapping("/users")
